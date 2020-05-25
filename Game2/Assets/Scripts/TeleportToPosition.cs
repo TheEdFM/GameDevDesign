937008c3f2918982ab69fb4c2d56c465a256105b
TheEdFM <47668458+TheEdFM@users.noreply.github.com>
2020-05-26 00:50:30 +0100

    Camera+TPs

---
+++ Game2/Assets/Scripts/TeleportToPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToPosition : MonoBehaviour
+{
+    public GameObject tpPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        player.transform.position = tpPoint.transform.position;
+    }
+}
